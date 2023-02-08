46922484e3bb15475fd18a103765badc0cf3eb9b
anonymous <anonymous>
2023-02-08 10:29:53 +0800

    处理嵌套对象

--- example/models.py
+++ example/models.py
@@ -1,4 +1,5 @@
 from django.db import models
+from datetime import datetime
 
 
 class FileSys(models.Model):
@@ -7,3 +8,9 @@ class FileSys(models.Model):
     file_size = models.IntegerField(max_length=4)
     first_upload_time = models.DateTimeField()
     last_upload_time = models.DateTimeField()
+
+
+class Comment(models.Model):
+    email = models.EmailField()
+    content = models.CharField(max_length=200)
+    created = models.DateTimeField(default=datetime.now())
--- example/serializer.py
+++ example/serializer.py
@@ -1,6 +1,7 @@
 from rest_framework import serializers
 
-from example.models import FileSys
+from example.models import FileSys, Comment
+from snippets.serializers import UserSerializer
 
 
 class ExampleSerializer(serializers.HyperlinkedModelSerializer):
@@ -10,3 +11,20 @@ class ExampleSerializer(serializers.HyperlinkedModelSerializer):
         model = FileSys
         # fields = ['filename', 'uploader', 'file_size', 'first_upload_time', 'last_upload_time']
         fields = 'all'
+
+
+class CommentSerializer(serializers.Serializer):
+    user = UserSerializer()
+    email = serializers.EmailField()
+    content = serializers.CharField(max_length=200)
+    created = serializers.DateTimeField()
+
+    def create(self, validated_data):
+        return Comment(**validated_data)
+
+    def update(self, instance, validated_data):
+        instance.email = validated_data.get('email', instance.email)
+        instance.content = validated_data.get('content', instance.content)
+        instance.created = validated_data.get('created', instance.created)
+        instance.save()
+        return instance
